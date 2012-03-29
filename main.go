e08af13db2a0cdbe7bfa8033ad62cfcc21bd313d
Joel Stemmer <stemmertech@gmail.com>
2012-03-29 17:18:04 +0200

    Update metatags

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 	"sort"
 )
 
-const VERSION = "0.0.1"
+const (
+	VERSION      = "0.0.1"
+	NAME         = "gotags"
+	URL          = "https://github.com/jstemmer/gotags"
+	AUTHOR_NAME  = "Joel Stemmer"
+	AUTHOR_EMAIL = "stemmertech@gmail.com"
+)
 
 var (
 	sortOutput bool
@@ -73,9 +79,10 @@ func createMetaTags() []string {
 	}
 	return []string{
 		"!_TAG_FILE_FORMAT\t2\t",
-		fmt.Sprintf("!_TAG_FILE_SORTED\t%d\t", sorted),
-		"!_TAG_PROGRAM_NAME\tgotags\t",
-		"!_TAG_PROGRAM_URL\thttps://github.com/jstemmer/gotags\t",
+		fmt.Sprintf("!_TAG_FILE_SORTED\t%d\t/0=unsorted, 1=sorted/", sorted),
+		fmt.Sprintf("!_TAG_PROGRAM_AUTHOR\t%s\t/%s/", AUTHOR_NAME, AUTHOR_EMAIL),
+		fmt.Sprintf("!_TAG_PROGRAM_NAME\t%s\t", NAME),
+		fmt.Sprintf("!_TAG_PROGRAM_URL\t%s\t", URL),
 		fmt.Sprintf("!_TAG_PROGRAM_VERSION\t%s\t", VERSION),
 	}
 }
