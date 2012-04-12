bf6914282d4a1e5c28b8e7667c5b753854d1f8b1
Joel Stemmer <stemmertech@gmail.com>
2012-04-12 10:32:26 +0200

    Fix version number

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	VERSION      = "1.0.0"
+	VERSION      = "1.1.0"
 	NAME         = "gotags"
 	URL          = "https://github.com/jstemmer/gotags"
 	AUTHOR_NAME  = "Joel Stemmer"
